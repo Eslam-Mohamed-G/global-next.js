8cc43c6a0c1fc6e3fea8e8e403c1819de8785372
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-07-03 20:27:19 +0200

    add works page

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Works = () => {
     return (
         <div>
-            <h1 className='text-white'>work</h1>
+            <h1 className='text-white font-bold text-3xl'>Selected work</h1>
         </div>
     );
 }
