749416b34f164ad50cfa2529866b820fbda3929d
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-07-03 20:33:37 +0200

    add link in navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,11 @@
+'use client'
 import React from 'react';
 import Image from "next/image";
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
 const Navbar = () => {
+    const pathName = usePathname();
     return (
         <nav className="flex flex-row justify-between items-center text-white py-5 px-5 md:px-12 xl:px-20">
             {/* logo */}
@@ -9,6 +13,8 @@ const Navbar = () => {
                 <Image src="/img/logo.png" width={30} height={30} alt={"logo"} />
                 <span className='uppercase font-bold text-white'>global</span>
             </div>
+            
+            <Link href="/hire" className={pathName === "/" ? 'hidden':'block bg-blueColor px-4 py-1 text-white'}>Hire us</Link>
 
             <button data-collapse-toggle="mega-menu-icons" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg cursor-pointer">
                 <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
