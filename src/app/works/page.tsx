d794eeb74b441d32b3aa7c578f7f413c1ce29c5d
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-07-04 15:57:52 +0200

    add react slick

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import Slider from "react-slick";
 
 const Works = () => {
+    const settings = {
+        className: "center",
+        centerMode: true,
+        infinite: true,
+        centerPadding: "60px",
+        slidesToShow: 3,
+        speed: 500
+      };
     return (
         <div>
             <h1 className='text-white font-bold text-3xl'>Selected work</h1>
