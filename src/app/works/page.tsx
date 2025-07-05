0fc4a353896ddc994b071bf5424ef301fcd2b483
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-07-05 18:33:53 +0200

    CustomArrows to slick-carouse;

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -4,6 +4,18 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+
+function SampleNextArrow(props: any) {
+    const { className, onClick } = props;
+    return (
+        <div onClick={onClick} className={className}>
+            <svg className="w-4 h-4 text-white relative z-50" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M1 5h12m0 0L9 1m4 4L9 9" />
+            </svg>
+        </div>
+    );
+}
+
 const Works = () => {
     const settings = {
         className: "center",
@@ -20,12 +32,13 @@ const Works = () => {
                     slidesToScroll: 1,
                 }
             },
-        ]
+        ],
+        nextArrow: <SampleNextArrow />,
     };
     return (
         <div className="flex flex-col content-center items-center justify-center justify-items-center text-white">
             <h1 className='font-bold text-4xl'>Selected work</h1>
-            <div className="h-2/3 w-80 xsm:w-96 sm:w-lg xmd:w-xl md:w-2xl xlg:w-4xl xl:w-5xl 2xl:w-6xl content-center">
+            <div className="h-2/3 w-80 xsm:w-96 sm:w-lg xmd:w-xl md:w-2xl xlg:w-4xl xl:w-5xl 2xl:w-6xl content-center border">
                 <Slider {...settings}>
                     <div className='h-full w-full '>
                         <div className="flex flex-col gap-5 items-center justify-center h-full w-full">
@@ -63,45 +76,6 @@ const Works = () => {
                     </div>
                 </Slider>
             </div>
-
-            {/* <div className="slider-container w-2xl">
-                <Slider {...settings}>
-                    <div className='h-96 w-1/3 border'>
-                        <div className="flex flex-col gap-5 items-center justify-center h-full w-full">
-                            <div className='w-44 h-44 rounded-full overflow-hidden'>
-                                <img src="/img/work-victory.jpg" alt="work-victory" className='w-full h-full' />
-                            </div>
-
-                            <div className='text-white'>
-                                <h1 className='uppercase font-bold'>victory</h1>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className='h-96 w-1/3 border'>
-                        <div className="flex flex-col gap-5 h-full items-center justify-center">
-                            <div className='w-80 h-80 rounded-full overflow-hidden'>
-                                <img src="/img/work-metiew-smith.jpg" alt="work-metiew-smith" className='w-full h-full' />
-                            </div>
-                            <div className='text-white'>
-                                <h1 className='uppercase font-bold'>metiew & smith</h1>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className='h-96 w-1/3 border'>
-                        <div className="flex flex-col gap-5 h-full items-center justify-center">
-                            <div className='w-44 h-44 rounded-full overflow-hidden'>
-                                <img src="/img/work-alex-nowak.jpg" alt="work-alex-nowak" className='w-full h-full' />
-                            </div>
-
-                            <div className='text-white'>
-                                <h1 className='uppercase font-bold'>alex nowax</h1>
-                            </div>
-                        </div>
-                    </div>
-                </Slider>
-            </div> */}
         </div>
     );
 }
