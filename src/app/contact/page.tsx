dfb4f89801cdebcfaa083e3884b48fc7099b1d01
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-07-06 18:48:06 +0200

    add contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Page = () => {
     return (
-        <div className='w-full h-full border border-red-400'>
+        <div className='w-full h-full'>
             <img src="/img/contact-visual.png" alt="contact-visual"  className='w-full h-full object-cover absolute start-0 end-0 top-0 bottom-0 -z-10'/>
         </div>
     );
