6c44f86ddf7235f0f0d102878539c1689eeeda88
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-07-07 14:57:50 +0200

    add hire page

---
+++ src/app/hire/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div className=''>
+            <h1 className='font-bold text-white text-3xl'>You wont us to do</h1>
+            
+        </div>
+    );
+}
+
+export default Page;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = () => {
                 <span className='uppercase font-bold text-white'>global</span>
             </div>
             
-            <Link href="/hire" className={pathName === "/" ? 'hidden':'block bg-blueColor px-4 py-0.5 text-white uppercase font-bold'}>Hire us</Link>
+            <Link href="/hire" className={pathName === "/" || pathName === "/hire" ? 'hidden':'block bg-blueColor px-4 py-0.5 text-white uppercase font-bold'}>Hire us</Link>
 
             <button data-collapse-toggle="mega-menu-icons" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg cursor-pointer">
                 <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
