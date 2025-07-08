887e2bd8bfdc403dbc6ff937eb497bdedf117e0f
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-07-08 18:19:08 +0200

    Responsive design

--- src/app/hire/page.tsx
+++ src/app/hire/page.tsx
@@ -2,21 +2,23 @@ import React from 'react';
 
 const Page = () => {
     return (
-        <div className='border border-red-600 w-full flex flex-col items-center justify-center gap-10'>
+        <div className='border border-red-600 w-full h-full flex flex-col items-center justify-center gap-10'>
             <h1 className='font-bold text-white text-3xl'>You wont us to do</h1>
             <div className="flex flex-col justify-center items-center gap-10">
                 <div className="text-white flex flex-row md:flex-col gap-3 xsm:gap-6 md:gap-10 items-center justify-center border">
                     <div className='flex flex-col md:flex-row gap-3 xsm:gap-6 md:gap-10'>
-                        <button className='font-bold xsm:text-[20px] capitalize border-2 hover:bg-blueColor transition-all delay-150 cursor-pointer w-40 xsm:w-48 md:w-56 py-3'>app design</button>
-                        <button className='font-bold xsm:text-[20px] capitalize border-2 hover:bg-blueColor transition-all delay-150 cursor-pointer w-40 xsm:w-48 md:w-56 py-3'>graphic design</button>
-                        <button className='font-bold xsm:text-[20px] capitalize border-2 hover:bg-blueColor transition-all delay-150 cursor-pointer w-40 xsm:w-48 md:w-56 py-3'>motion design</button>
+                        <button className='font-bold xsm:text-[20px] capitalize border-2 hover:bg-blueColor transition-all delay-150 cursor-pointer w-40 xsm:w-48 md:w-52 lg:w-56 py-3'>app design</button>
+                        <button className='font-bold xsm:text-[20px] capitalize border-2 hover:bg-blueColor transition-all delay-150 cursor-pointer w-40 xsm:w-48 md:w-52 lg:w-56 py-3'>graphic design</button>
+                        <button className='font-bold xsm:text-[20px] capitalize border-2 hover:bg-blueColor transition-all delay-150 cursor-pointer w-40 xsm:w-48 md:w-52 lg:w-56 py-3'>motion design</button>
                     </div>
                     <div className='flex flex-wrap flex-col md:flex-row items-center justify-center gap-3 xsm:gap-6 md:gap-10 max-w-2xl'>
-                        <button className='font-bold xsm:text-[20px] capitalize border-2 hover:bg-blueColor transition-all delay-150 cursor-pointer w-40 xsm:w-48 md:w-56 py-3'>ux design</button>
-                        <button className='font-bold xsm:text-[20px] capitalize border-2 hover:bg-blueColor transition-all delay-150 cursor-pointer w-40 xsm:w-48 md:w-56 py-3'>web design</button>
-                        <button className='font-bold xsm:text-[20px] capitalize border-2 hover:bg-blueColor transition-all delay-150 cursor-pointer w-40 xsm:w-48 md:w-56 py-3'>marketing</button>
+                        <button className='font-bold xsm:text-[20px] capitalize border-2 hover:bg-blueColor transition-all delay-150 cursor-pointer w-40 xsm:w-48 md:w-52 lg:w-56 py-3'>ux design</button>
+                        <button className='font-bold xsm:text-[20px] capitalize border-2 hover:bg-blueColor transition-all delay-150 cursor-pointer w-40 xsm:w-48 md:w-52 lg:w-56 py-3'>web design</button>
+                        <button className='font-bold xsm:text-[20px] capitalize border-2 hover:bg-blueColor transition-all delay-150 cursor-pointer w-40 xsm:w-48 md:w-52 lg:w-56 py-3'>marketing</button>
                     </div>
                 </div>
+
+                {/* form */}
                 <div className="w-full">
                     <form action="" className=' flex flex-col justify-center gap-10'>
                         <div className="flex flex-col md:flex-row md:gap-10">
